5221e8cd8946af5200149b18f0fe61e82929332f
LizClaudia <84141610+20203tn128@users.noreply.github.com>
2023-06-08 14:57:10 -0600

    Cambios js

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
 console.log("Hola desde function js")
 const tv={
-    power:true,
+    power:false,
     volumen:0,
     canal:0,
-    silence:true
+    silence:false
 }
 
 const powerButton = document.getElementById("OnOffButton");
@@ -12,40 +12,45 @@ const vol2Button = document.getElementById("vol2Button");
 const canal1Button = document.getElementById("canal1Button");
 const canal2Button = document.getElementById("canal2Button");
 
+function print(){
+    console.log("El estado actual del televisor es: \n" + "encender: " + tv.power + "\n" + "volumen: " + tv.volumen + "\n" + "canal: " + tv.canal + "\n" + "silencio: " + tv.silence + "\n"  )
+}
 
 function  turnOn(){
-    if(tv.power){
+    if(!tv.power){
         console.log("A encendido el televisor");
         tv.volumen = 3;
         tv.canal = 7;
-        tv.power = false;
-        console.log(tv)
+        tv.power = true;
+        print();
     }else{
         console.log("A apagado el televisor");
-        tv.power = true;
+        tv.power = false;
         tv.volumen = 0;
         tv.canal= 0;
         tv.silence = false;
-        console.log(tv)
+        print();
 
     }
 }
 
 
 function silence(){
-    if(!tv.power){
+    if(tv.power){
         let volumen = tv.volumen;
-        if(tv.silence){
+        if(!tv.silence){
             console.log("Has activado el modo silencio");
             tv.volumen = 0;
-            console.log(tv)
+            tv.silence = true; 
+            print();
             tv.volumen = volumen
-            tv.silence = false;
+            
         }else{
             console.log("Has salido del modo silencio");
             tv.volumen = volumen;
-            console.log(tv)
-            tv.silence = true;
+            tv.silence = false;
+            print();
+            
         }
     }else{
         console.log("La television no esta prendida");
@@ -53,13 +58,15 @@ function silence(){
 }
 
 function soundHard(){
-    if(!tv.power && tv.silence){
+    if(tv.power){
             if(tv.volumen < 10){
                 tv.volumen++;
                 console.log("Has subido el volumen a: " + tv.volumen);
+                print();
              }else{
                 if(tv.volumen == 10)
                 console.log("Has llegado al maximo del volumen " + tv.volumen)
+                print();
              }
              
     }else{
@@ -68,13 +75,15 @@ function soundHard(){
 }
 
 function soundDown(){
-    if(!tv.power  && tv.silence){
+    if(tv.power  && !tv.silence){
         if(tv.volumen > 1){
             tv.volumen--;
             console.log("Has bajado el volumen a: " + tv.volumen);
+            print();
          }else{
             if(tv.volumen == 1)
             console.log("Has llegado al minimo del volumen " + tv.volumen)
+            print();
          }
          
 }else{
@@ -87,9 +96,11 @@ function changeHard(){
         if(tv.canal < 100){
             tv.canal++;
             console.log("Has subido el canal a: " + tv.canal);
+            print();
          }else{
             if(tv.canal == 100)
             console.log("Has llegado al maximo del canal " + tv.canal)
+            print();
          }
          
 }else{
@@ -102,9 +113,11 @@ function changeDown(){
         if(tv.canal > 1){
             tv.canal--;
             console.log("Has bajado el canal a: " + tv.canal);
+            print();
          }else{
             if(tv.canal == 1)
             console.log("Has llegado al minimo del canal " + tv.canal)
+            print();
          }
          
 }else{
